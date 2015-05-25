eb4070d93e83d5c7ca5654c71dea6cc3a410ef27
duzitong <duzitong811@126.com>
2015-05-25 22:20:54 +0800

    header update

--- DES/DES.h
+++ DES/DES.h
@@ -4,3 +4,9 @@
 
 typedef unsigned char u8;
 typedef unsigned int u32;
+
+void rotateL(u8*, u8*, u8);
+void getRoundKeys(u8*, u8*);
+void roundFunction(u8*, u8*, u8*);
+void encrypt(u8*, u8*, u8*);
+void decrypt(u8*, u8*, u8*);
